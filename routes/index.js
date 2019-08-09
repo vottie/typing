9be3202ca93dcde14c036a1347665f3ba14c2caa
vottie <tsuboi23@gmail.com>
2019-08-10 03:18:27 +0900

    Added express, passport.

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+var passport = require('passport');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { user: req.user });
+});
+
+router.get('/login', function(req, res) {
+  res.render('login', {user : req.user });
+});
+
+router.post('/login', passport.authenticate('local',
+  {successRedirect: '/',
+  failureRedirect: '/login',
+  session: false}));
+
+router.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+module.exports = router;
