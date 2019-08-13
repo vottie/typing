28577ca4b9f29ce83a4443164dc261d7263ad28b
vottie <tsuboi23@gmail.com>
2019-08-14 03:04:14 +0900

    Use mongoose.

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ var logger = require('morgan');
 
 // added
 var passport = require('passport');
-var LocalStrategy = require('passport-local');
+var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,24 +26,53 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// added mongoose
+mongoose.Promise = global.Promise;
+mongoose.connect("mongodb://127.0.0.1:27017/user_test", {useNewUrlParser: true});
+app.use(session({
+//  secret: this.defaultConfiguration.secret.session,
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true,
+  store: new MongoStore({mongooseConnection: mongoose.connection})
+}));
 // added
 app.use(passport.initialize());
-var LocalStrategy = require('passport-local').Strategy;
-passport.use(new LocalStrategy({
-  usernameField: 'username',
-  passwordField: 'password',
-  passReqToCallback: true,
-  session: false,
-}, function (req, username, password, done) {
-  process.nextTick(function () {
-    if (username === "test" && password === "test") {
-      return done(null, username)
-    } else {
-      console.log("login error")
-      return done(null, false, {message : 'パスワードが正しくありません'})
-    }
-  })
-}));
+app.use(passport.session);
+
+var User = require("./models/user");
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use(function(req,res,next){
+  res.locals = {
+      user: req.user
+  };
+  next();
+});
+
+var index = require('./routes/index');
+var user = require('./routes/users');
+
+app.use('/index',index);
+app.use('/users',user);
+
+//passport.use(new LocalStrategy({
+//  usernameField: 'username',
+//  passwordField: 'password',
+//  passReqToCallback: true,
+//  session: false,
+//}, function (req, username, password, done) {
+//  process.nextTick(function () {
+//    if (username === "test" && password === "test") {
+//    return done(null, username)
+//    } else {
+//      console.log("login error")
+//      return done(null, false, {message : 'パスワードが正しくありません'})
+//    }
+//  })
+//}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
